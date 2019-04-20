b6bbfbabe5cf522034ba5c913c92734262e06952
Jim Thompson <jimthompson5802@aol.com>
2019-04-20 16:27:46 -0400

    allow spark worker parameters to be specifed in d-c.yaml

--- docker/worker/start-spark-worker.sh
+++ docker/worker/start-spark-worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/spark/sbin/start-slave.sh spark://master:7077  --cores 4  --memory 4g
+/usr/local/spark/sbin/start-slave.sh spark://master:7077  $@
 
 # keep this script alive so container stays active
 while [ 1 -eq 1 ]
