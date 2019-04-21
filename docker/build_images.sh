25b02aee024de18ebea009309217e559a9a8a4cb
Jim Thompson <jimthompson5802@aol.com>
2019-04-20 22:22:30 -0400

    updated

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -8,13 +8,13 @@ PY4J_VERSION=0.10.7
 docker build --build-arg APACHE_SPARK_VERSION=$APACHE_SPARK_VERSION \
     --build-arg HADOOP_VERSION=$HADOOP_VERSION \
     --build-arg PY4J_VERSION=$PY4J_VERSION \
-    -t spark-base:$APACHE_SPARK_VERSION docker/base
+    -t spark-base:$APACHE_SPARK_VERSION base
 
 docker build \
-    -t spark-master:$APACHE_SPARK_VERSION docker/master
+    -t spark-master:$APACHE_SPARK_VERSION master
 
 docker build \
-    -t spark-worker:$APACHE_SPARK_VERSION docker/worker
+    -t spark-worker:$APACHE_SPARK_VERSION worker
 
 docker build \
-    -t spark-pyspnb:$APACHE_SPARK_VERSION docker/pyspnb
+    -t spark-pyspnb:$APACHE_SPARK_VERSION pyspnb
