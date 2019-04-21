af65367ea5a8cc4eba680ad19cd0c297912f9184
Jim Thompson <jimthompson5802@aol.com>
2019-04-21 13:42:04 -0400

    rationalized container host names

--- docker/pyspnb/start-pyspnb.sh
+++ docker/pyspnb/start-pyspnb.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 
-pyspark --master spark://master:7077
+
+PYSPARK_DRIVER_PYTHON="jupyter" \
+PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.token='' --notebook-dir=/opt/project \
+    --allow-root --ip='*'  --port=8888 --no-browser" \
+pyspark --master spark://spark-master:7077
--- docker/worker/start-spark-worker.sh
+++ docker/worker/start-spark-worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/local/spark/sbin/start-slave.sh spark://master:7077  $@
+/usr/local/spark/sbin/start-slave.sh spark://spark-master:7077  $@
 
 # keep this script alive so container stays active
 while [ 1 -eq 1 ]
