18562472f4645458c59259c15800ca6630f4779b
Jim Thompson <jimthompson5802@aol.com>
2019-04-30 22:03:48 -0400

    completed port forwarding helper

--- spark-cluster/k8s/port_forward_helper.py
+++ spark-cluster/k8s/port_forward_helper.py
@@ -1,31 +1,67 @@
 """
 Extracts kubernetes port information to setup port forwarding to allow web access
+
+python port_forward_helper.py  setup|teardown
+
 """
 
 import yaml
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, DEVNULL
+import argparse
+import os
 
-K8S_GET_PODS = 'kubectl get pods -o yaml'
+PID_LIST_FILE = './port-forward-pids'
 
-# Retrieve pod configuration info
-cmd = Popen(K8S_GET_PODS,shell=True,stdout=PIPE)
-cmd.wait()
 
-# convert yaml to python dictionary
-pods_dict = yaml.safe_load(cmd.stdout)
+parser = argparse.ArgumentParser(description='Port forwarding helper.')
 
-# loop through all pods looking for ports to forward and build port forwarding
-for pod in pods_dict['items']:
+parser.add_argument('operation', nargs='?', choices=['setup', 'teardown'],
+                    default='setup',
+                    help="'setup' or 'teardown' port forwarding")
 
-    # start building the port forward command
-    port_forward_cmd = 'kubectl port-forward ' + pod['metadata']['name']
+args = parser.parse_args()
 
-    # for each container in pod extract ports
-    for c in pod['spec']['containers']:
-        for p in c['ports']:
-            port_forward_cmd += ' ' + str(p['containerPort'])
+if args.operation == 'setup':
+    # setup port forwarding
 
-    # invoke port-forward command
-    print('invoke command',port_forward_cmd)
-    cmd = Popen(port_forward_cmd + '&', shell=True)
+    K8S_GET_PODS = 'kubectl get pods -o yaml'
+
+    # Retrieve pod configuration info
+    cmd = Popen(K8S_GET_PODS,shell=True,stdout=PIPE)
     cmd.wait()
+
+    # convert yaml to python dictionary
+    pods_dict = yaml.safe_load(cmd.stdout)
+
+    # loop through all pods looking for ports to forward and build port forwarding
+    pid_string = ""
+    for pod in pods_dict['items']:
+
+        # start building the port forward command
+        port_forward_cmd = 'kubectl port-forward ' + pod['metadata']['name']
+
+        # for each container in pod extract ports
+        for c in pod['spec']['containers']:
+            for p in c['ports']:
+                port_forward_cmd += ' ' + str(p['containerPort'])
+
+        # invoke port-forward command
+        print('invoke command',port_forward_cmd)
+        cmd = Popen(port_forward_cmd, shell=True, stdout=DEVNULL)
+        print('started pid:', cmd.pid)
+        pid_string += str(cmd.pid) + " "
+
+    # write port-forwarding pids to file for teardown
+    with open(PID_LIST_FILE,"w") as f:
+        f.write(pid_string)
+
+else:
+
+    # tear down the port-forwarding processes
+    with open(PID_LIST_FILE,'r') as f:
+        pid_string = f.read()
+
+    Popen("kill " + pid_string,shell=True)
+
+    os.remove(PID_LIST_FILE)
+
